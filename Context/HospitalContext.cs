c46f8e631a0a9c20fb3ef3a07ba8d1f69829905d
Andrissh10 <guganandris056@gmail.com>
2024-11-05 12:36:01 +0100

    Add project files.

---
+++ Context/HospitalContext.cs
@@ -0,0 +1,17 @@
+﻿using HospitalPatientAPI.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace HospitalPatientAPI.Context
+{
+    public class HospitalContext : DbContext
+    {
+        public HospitalContext(DbContextOptions<HospitalContext>options) : base(options)
+        {
+        }
+
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Patient>  Patients { get; set; }
+        public DbSet<Treatment> Treatments { get; set; }
+    }
+}
